fd5f154eb4a019ead854320cac05e6218929929c
yutanov <utanov@gmail.com>
2022-07-13 23:42:40 +0300

    add tasks

--- task_one.py
+++ task_one.py
@@ -1,3 +1,8 @@
 def task(array: str):
     el_index = array.index("0")
     return el_index
+
+
+if __name__ == "__main__":
+    string = "111111111110000000000000000"
+    assert task(string) == 11
