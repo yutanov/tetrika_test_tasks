67f1ce7f1c1c5f1e225d79c341f423a8c739ebee
yutanov <utanov@gmail.com>
2022-07-14 10:31:48 +0300

    add task

--- task_two.py
+++ task_two.py
@@ -6,11 +6,9 @@ def task_two():
 
     def get_animals(page=None, list_of_animals=None):
         '''
-        Функция производит сбор всех перечисленных животных на русском языке, до тех пор,
-        пока не встречаестся перечень животных на латыни, первое из них "Aaaaba"
+        Функция производит сбор всех перечисленных животных на русском языке и латыни
         '''
 
-        stop_name = "Aaaaba"
 
         if page is None:
             url = "https://inlnk.ru/jElywR"
@@ -26,13 +24,10 @@ def task_two():
         '''
         animals = soup.find('div', id='mw-pages').find_all('a')
         for animal in animals:
-            if animal.text not in navigation_links and animal.text != stop_name:
+            if animal.text not in navigation_links:
                 list_of_animals.append(animal.text)
                 count = len(list_of_animals)
                 print(f'Collected {count} animals.')
-
-            elif animal.text == stop_name:
-                return list_of_animals
 
         '''
         Переход на следуюущую страницу
@@ -53,7 +48,11 @@ def task_two():
         count_dict = {"А": 0, "Б": 0, "В": 0, "Г": 0, "Д": 0, "Е": 0, "Ё": 0,
                       "Ж": 0, "З": 0, "И": 0, "Й": 0, "К": 0, "Л": 0, "М": 0,
                       "Н": 0, "О": 0, "П": 0, "Р": 0, "С": 0, "Т": 0, "У": 0,
-                      "Ф": 0, "Х": 0, "Ц": 0, "Ч": 0, "Ш": 0, "Щ": 0, "Э": 0, "Ю": 0, "Я": 0}
+                      "Ф": 0, "Х": 0, "Ц": 0, "Ч": 0, "Ш": 0, "Щ": 0, "Э": 0, "Ю": 0, "Я": 0,
+                      "A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0, "G": 0,
+                      "H": 0, "I": 0, "J": 0, "K": 0, "L": 0, "M": 0, "N": 0,
+                      "O": 0, "P": 0, "Q": 0, "R": 0, "S": 0, "T": 0, "U": 0,
+                      "V": 0, "W": 0, "X": 0, "Y": 0, "Z": 0}
 
         for el in list_of_names:
             key = el[0]
@@ -70,3 +69,7 @@ def task_two():
     list_of_animals = get_animals()
     count_string = count(list_of_animals)
     return count_string
+
+
+if __name__ == "__main__":
+    print(task_two())
